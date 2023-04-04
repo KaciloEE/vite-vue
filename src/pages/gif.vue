438d50c49d118f4f92cc3bff4436bc393d886e7f
KaciloEE <KaciloEE@gmail.com>
2023-04-05 00:49:03 +0200

    first commit

---
+++ src/pages/gif.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <router-link to="/">Back Home Page</router-link>
+    <br>
+    <h2>{{ item.title }}</h2>
+    <img
+      :src="item?.images?.fixed_height?.url"
+      :height="item?.images?.fixed_height?.height"
+      :width="item?.images?.fixed_height?.width"
+      alt=""
+    />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      item: {},
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/${this.$route.params.id}?api_key=XQJsiGIND0fn0MgbDhlEPcgKxR7DykpN`
+        );
+
+        const data = await response.json();
+        this.item = data.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+// https://api.giphy.com/v1/gifs/G0OM2OGY2F6i0rfE1g?api_key=XQJsiGIND0fn0MgbDhlEPcgKxR7DykpN
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/router.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Gif from "./pages/gif.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "chat",
+      component: () => import("./pages/index.vue"),
+    },
+    {
+      path: "/gif/:id",
+      name: "gif",
+      component: () => import("./pages/gif.vue"),
+    },
+  ],
+});
+
+export default router;
